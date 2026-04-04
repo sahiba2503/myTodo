c0ab79efe1ac409405a452581c63d222aea285ed
sahiba <25sahiba03@gmail.com>
2026-04-04 22:47:02 +0530

    add task create section

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,29 @@
-import React from 'react'
 
+import { useState } from 'react'  
 const App = () => {
+  const[todo, setTodo]= useState("");  
+  const[task,setTask]=useState([]);
+  function handleSubmit(e){
+e.preventDefault();
+setTask([...task,todo]);
+setTodo("");
+  }
   return (
+
     <div>
-     
-     <h2> welcome to my website.</h2>
+      <h2>Todo Tasks : {todo}</h2>
+      <form onSubmit={handleSubmit}>
+        <input type="text"
+        value={todo}
+        onChange={(e)=>setTodo(e.target.value)} />
+        <button type = "submit" >Create</button>
+      </form>
+ <ul>
+  {task.map((name, index)=>(
+    <li key={index}>{name}</li>
+  ))
+}
+ </ul>
     </div>
   )
 }
